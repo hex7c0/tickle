badc9b846ae8576c06fd770fc9fabc24e484f2db
hex7c0 <carniellifrancesco@gmail.com>
2014-06-18 21:19:14 +0200

    1.0.2

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
  * @module tickle
  * @package tickle
  * @subpackage main
- * @version 1.0.1
+ * @version 1.0.2
  * @author hex7c0 <hex7c0@gmail.com>
  * @copyright hex7c0 2014
  * @license GPLv3
@@ -15,6 +15,51 @@
  */
 if (!GLOBAL.tickle) {
     GLOBAL.tickle = new tickle;
+    /**
+     * reset all routing counter
+     * 
+     * @function reset
+     * @return
+     */
+    tickle.prototype.reset = function() {
+
+        this.all = 0;
+        var route = this.route;
+        for ( var property in route) {
+            route[property] = 0;
+        }
+        return;
+    };
+    /**
+     * increase counter
+     * 
+     * @function add
+     * @param {String} path - url path
+     * @return {Integer}
+     */
+    tickle.prototype.add = function(path) {
+
+        ++this.all;
+        var plus = ++this.route[path];
+        if (!plus) {
+            plus = this.route[path] = 1;
+        }
+        return plus;
+    };
+    /**
+     * time per request
+     * 
+     * @function tpr
+     * @return {Float}
+     */
+    tickle.prototype.tpr = function() {
+
+        var time = this.time;
+        var diff = process.hrtime(time[0]);
+        var all = this.all;
+        this.time = [process.hrtime(),all];
+        return ((diff[0] * 1e9 + diff[1]) / 1000000) / (all - time[1]);
+    };
 }
 
 /*
@@ -32,50 +77,6 @@ function tickle() {
     this.route = {};
 };
 /**
- * reset all routing counter
- * 
- * @function reset
- * @return
- */
-tickle.prototype.reset = function() {
-
-    this.all = 0;
-    var route = this.route;
-    for ( var property in route) {
-        route[property] = 0;
-    }
-    return;
-};
-/**
- * increase counter
- * 
- * @function add
- * @param {String} path - url path
- * @return {Integer}
- */
-tickle.prototype.add = function(path) {
-
-    var plus = ++this.route[path];
-    if (!plus) {
-        plus = this.route[path] = 1;
-    }
-    return plus;
-};
-/**
- * time per request
- * 
- * @function tpr
- * @return {Float}
- */
-tickle.prototype.tpr = function() {
-
-    var time = this.time;
-    var diff = process.hrtime(time[0]);
-    var all = this.all;
-    this.time = [process.hrtime(),all];
-    return ((diff[0] * 1e9 + diff[1]) / 1000000) / (all - time[1]);
-};
-/**
  * body
  * 
  * @exports main
@@ -88,7 +89,6 @@ tickle.prototype.tpr = function() {
 module.exports = function(req,res,next) {
 
     var globo = GLOBAL.tickle;
-    globo.all++;
     req.tickle = globo.add(req.url);
     try {
         return next();
