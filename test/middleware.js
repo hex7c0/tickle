e6a142a1186e211678275e67e8e007b2e195a0bf
hex7c0 <carniellifrancesco@gmail.com>
2014-11-18 14:49:34 +0100

    test updated

--- test/middleware.js
+++ test/middleware.js
@@ -14,70 +14,78 @@
  */
 // import
 try {
-    var tickle = require('../index.min.js'); // use require('tickle')
-    var app = require('express')();
-    var request = require('supertest');
-    var assert = require('assert');
+  var tickle = require('..'); // use require('tickle')
+  var app = require('express')();
+  var request = require('supertest');
+  var assert = require('assert');
 } catch (MODULE_NOT_FOUND) {
-    console.error(MODULE_NOT_FOUND);
-    process.exit(1);
+  console.error(MODULE_NOT_FOUND);
+  process.exit(1);
 }
 
 /*
  * test module
  */
-describe('middleware',function() {
+describe('middleware', function() {
 
-    before(function(done) {
+  before(function(done) {
 
-        app.use(tickle);
-        app.get('/',function(req,res) {
+    app.use(tickle);
+    app.get('/', function(req, res) {
 
-            res.send(String(req.tickle));
-        });
-        app.get('/a',function(req,res) {
+      res.send(String(req.tickle));
+    });
+    app.get('/a', function(req, res) {
 
-            res.send(String(req.tickle));
-        });
+      res.send(String(req.tickle));
+    });
+    done();
+  });
+
+  it('"/a" - should return 1', function(done) {
+
+    request(app).get('/a').expect(200).end(function(err, res) {
+
+      if (err)
+        throw err;
+      assert.strictEqual(Number(res.text), 1);
+      done();
+    });
+  });
+
+  describe('multiple times', function() {
+
+    it('"/" - 1°', function(done) {
+
+      request(app).get('/').expect(200, done);
+    });
+    it('"/" - 2°', function(done) {
+
+      request(app).get('/').expect(200, done);
+    });
+    it('"/" - should return 3', function(done) {
+
+      request(app).get('/').expect(200).end(function(err, res) {
+
+        if (err)
+          throw err;
+        assert.strictEqual(Number(res.text), 3);
         done();
+      });
     });
+    it('"/" - 4°', function(done) {
 
-    it('"/" - should return 3',function(done) {
-
-        request(app).get('/').expect(200).end(function(err,res) {
-
-            // pass
-        });
-        request(app).get('/').expect(200).end(function(err,res) {
-
-            // pass
-        });
-        request(app).get('/').expect(200).end(function(err,res) {
-
-            assert.strictEqual(Number(res.text),3);
-            done();
-        });
+      request(app).get('/').expect(200, done);
     });
+    it('"/" - should return 5', function(done) {
 
-    it('"/a" - should return 1',function(done) {
+      request(app).get('/').expect(200).end(function(err, res) {
 
-        request(app).get('/a').expect(200).end(function(err,res) {
-
-            assert.strictEqual(Number(res.text),1);
-            done();
-        });
+        if (err)
+          throw err;
+        assert.strictEqual(Number(res.text), 5);
+        done();
+      });
     });
-
-    it('"/" - should return 5',function(done) {
-
-        request(app).get('/').expect(200).end(function(err,res) {
-
-            // pass
-        });
-        request(app).get('/').expect(200).end(function(err,res) {
-
-            assert.strictEqual(Number(res.text),5);
-            done();
-        });
-    });
+  });
 });
